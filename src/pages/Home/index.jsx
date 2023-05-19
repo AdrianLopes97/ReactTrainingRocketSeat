b572084e18abe131f875a29187ed50657a32096d
cintiagab <adrian.qodeless@outlook.com>
2023-05-19 03:22:22 -0300

    files moving and create style

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,11 @@
-function Home() {
+import './styles.css'
 
+export const Home = () => {
   return (
-    <>
+    <div className='qodeless'>
       <h1>Qodeless - the 3 patetas</h1>
       <input type="text" placeholder="Digite o nome ..."/>
       <button type="button">Adicionar</button>
-    </>
+    </div>
   )
 }
-
-export default Home
